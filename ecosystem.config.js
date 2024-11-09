bac881a4a7fe67356d8e93f11f192c72b8a69e71
Andrew Lucas <andrewlucasgs@gmail.com>
2024-11-09 12:22:38 -0300

    Attempt to fix pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,9 @@
-export default {
-  apps: [
-      {
-          name: 'ProcessorDB-website',
-          port: '3000',
-          exec_mode: 'cluster',
-          instances: 'max',
-          script: './.output/server/index.mjs'
-      }
-  ]
-};
+module.exports = {
+  apps : [{
+    name: "ProcessorDB-website",
+    port: "3000",
+    exec_mode: "cluster",
+    instances: "max",
+    script: "./.output/server/index.mjs"
+}]
+}
