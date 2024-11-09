26b3c371ac563dfa3d94ae3b61457fdb7c75bdd8
Andrew Lucas <andrewlucasgs@gmail.com>
2024-11-09 12:13:06 -0300

    Add pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'ProcessorDB-website',
+        port: '3000',
+        exec_mode: 'cluster',
+        instances: 'max',
+        script: './.output/server/index.mjs'
+      }
+    ]
+  }
+  
