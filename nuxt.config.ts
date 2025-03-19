571391140f0b25c671dacf2a9fa5407031bdee78
zarbielli <jlfz06@gmail.com>
2025-03-19 15:41:47 -0300

    Improves build script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,18 @@ export default defineNuxtConfig({
   vite: {
     optimizeDeps: {
       include: ["plotly.js-dist-min"]
-    }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id: any) {
+            if (id.includes("node_modules")) {
+              return id.toString().split("node_modules/")[1].split("/")[0].toString();
+            }
+          },
+        },
+      },
+    },
   },
 
   plugins: [
